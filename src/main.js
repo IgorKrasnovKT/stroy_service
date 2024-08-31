a278a766427459bd46985ca6c3a54dae272244ae
Igor <igorkr2000@mail.ru>
2024-08-31 03:29:50 +0300

    gh pages

--- src/main.js
+++ src/main.js
@@ -3,8 +3,24 @@ import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <BrowserRouter>
+//     <App/>
+//   </BrowserRouter>
+// );
+
+// const root = createRoot(
+//   document.getElementById('root')
+// );
+// root.render(
+//   <BrowserRouter>
+//       <App/>
+//   </BrowserRouter>
+// );
+
+ReactDOM.render(
   <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
