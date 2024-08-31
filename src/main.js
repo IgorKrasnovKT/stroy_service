26a58e3dbc1d413f820cd021524259884cc4ecfd
Igor <igorkr2000@mail.ru>
2024-08-31 03:32:26 +0300

    gh pages

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
+import ReactDOM from 'react-dom';
 import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
