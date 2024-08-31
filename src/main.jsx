af40f0b24f410e1c94e1ec7eb91971121abe305e
Igor <igorkr2000@mail.ru>
2024-08-31 04:24:59 +0300

    gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,9 @@ import App from "./App.jsx";
 import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 
-console.log(import.meta.env.PUBLIC_URL)
+console.log(import.meta.env.VITE_PUBLIC_URL)
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={import.meta.env.PUBLIC_URL}>
+  <BrowserRouter basename={import.meta.env.VITE_PUBLIC_URL}>
     <App/>
   </BrowserRouter>
 );
